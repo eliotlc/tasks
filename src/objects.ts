602e3c3c73dabb16ec57a011a9386765dbd68f50
eliotlc <eliot.cole@maine.edu>
2026-02-17 20:19:13 -0500

    implemented toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -78,7 +78,17 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    return `# ${question.name}
+${question.body}${
+        question.type === "multiple_choice_question" ?
+            "\n" +
+            question.options
+                .map((option: string) => {
+                    return "- " + option;
+                })
+                .join("\n")
+        :   ""
+    }`;
 }
 
 /**
