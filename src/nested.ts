15a452359dae779f60db980845e8f01d504bf483
eliotlc <eliot.cole@maine.edu>
2026-02-23 20:32:14 -0500

    Implemented removeQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -44,7 +44,7 @@ export function findQuestion(
  * Hint: use filter
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    return questions.filter((q: Question) => q.id !== id);
 }
 
 /***
